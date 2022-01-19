9f27797cf0c00e226df5c45d176a592183abc812
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-01-19 18:53:56 -0500

    tratanto de corregir error con django

--- WAUDIT/settings.py
+++ WAUDIT/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = ''
-STATIC_ROOT=[BASE_DIR,'staticfiles']##OJO validar
+STATIC_ROOT=[BASE_DIR,'staticfiles']##OJO
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
