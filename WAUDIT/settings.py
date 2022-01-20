f796c2152ddf808d873d19eee84f844e0b137ac0
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-01-19 19:25:01 -0500

    modificar configuración del archivo settings

--- WAUDIT/settings.py
+++ WAUDIT/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from email.policy import default
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,13 +134,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = ''
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = ''
 # STATIC_ROOT=[BASE_DIR,'staticfiles']##OJO
+STATICFILE_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
