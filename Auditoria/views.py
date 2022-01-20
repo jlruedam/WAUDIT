e1c60534071158c38bdba50c9c5525da32df7c94
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-01-20 08:54:20 -0500

    modificar vistas auditoria

--- Auditoria/views.py
+++ Auditoria/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,"templates/index.html")
+    return render(request,"index.html")
     
 def GestionAuditoria(request):
     return render(request,"GestionAuditoria.html")
