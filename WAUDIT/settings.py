254dc62744630e5579ee035c91c990cef7a80b1e
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-01-20 16:55:07 -0500

    otro commit

--- WAUDIT/settings.py
+++ WAUDIT/settings.py
@@ -41,10 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Auditoria",
-    "Universo",
+    "Auditoria.apps.AuditoriaConfig",
+    "Universo.apps.UniversoConfig"
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
